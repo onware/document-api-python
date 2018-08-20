8ff63a030306e381f7add89ba14d70c9f9f34ccc
Sam Bao <sam.bao@onware.com>
2018-08-20 17:39:18 -0600

    TCS-152 #time 2h attempting to upload tableau workbook

--- samples/filter-and-replicate/filter-and-replicate.py
+++ samples/filter-and-replicate/filter-and-replicate.py
@@ -1,28 +1,113 @@
 import csv              # so we can work with our database list (in a CSV file)
 ############################################################
-# Step 1)  Use Workbook object from the Document API
+# Step 1) Use Workbook object from the Document API
 ############################################################
+import argparse
+import getpass
+import logging
+
+import tableauserverclient as TSC
+from tableauserverclient import ConnectionCredentials, ConnectionItem
+
+
 from tableaudocumentapi import Workbook
 
 ############################################################
-# Step 3)  Use a database list (in CSV), loop thru and
+# Step 3) Use a database list (in CSV), loop thru and
 #          create new .twb's with their settings
 ############################################################
-with open('databases.csv') as csvfile:
-    databases = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    for row in databases:
-			
-        # Open the workbook
-        sourceWB = Workbook(row['Workbook'] + row['Format'])
 
-        # Update the filters
-        for datasource in reversed(sourceWB.datasources):
-            for children in datasource._datasourceTree._root._children:
-				if "column" in children.attrib and "class" in children.attrib:
-					if children.attrib["column"] == "[Branch]" and children.attrib["class"] == "categorical":						
-						for subchildren in children._children:
-							if "member" in subchildren.attrib:
-								subchildren.attrib["member"] = '&quot;' + row['Branch'] + '&quot;'
-                    
-        # Save our newly created workbook with the new file name
-        sourceWB.save_as(row['Workbook'] + ' - ' + row['Branch'] + row['Format'])
+def main():
+
+    parser = argparse.ArgumentParser(description='Publish a workbook to server.')
+    parser.add_argument('--server', '-s', required=True, help='server address')
+    parser.add_argument('--username', '-u', required=True, help='username to sign into server')
+    parser.add_argument('--logging-level', '-l', choices=['debug', 'info', 'error'], default='error',
+                        help='desired logging level (set to error by default)')
+    parser.add_argument('--as-job', '-a', help='Publishing asynchronously', action='store_true')
+
+    args = parser.parse_args()
+
+    password = getpass.getpass("Password: ")
+
+    # Set logging level based on user input, or error by default
+    logging_level = getattr(logging, args.logging_level.upper())
+    logging.basicConfig(level=logging_level)
+
+    # Step 1: Sign in to server.
+    tableau_auth = TSC.TableauAuth(args.username, password)
+    server = TSC.Server(args.server)
+
+    overwrite_true = TSC.Server.PublishMode.Overwrite
+    with server.auth.sign_in(tableau_auth):
+        with open('databases.csv') as csvfile:
+            databases = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+            for row in databases:
+	    		
+            # Open the workbook
+                sourceWB = Workbook(row['Workbook'] + row['Format'])
+
+            # Update the filters
+                for datasource in reversed(sourceWB.datasources):
+                    for children in datasource._datasourceTree._root._children:
+	    	    		if "column" in children.attrib and "class" in children.attrib:
+	    	    			if children.attrib["column"] == "[Branch]" and children.attrib["class"] == "categorical":						
+	    	    				for subchildren in children._children:
+	    	    					if "member" in subchildren.attrib:
+	    	    						subchildren.attrib["member"] = '&quot;' + row['Branch'] + '&quot;'
+                        
+            # Save our newly created workbook with the new file name
+                sourceWB.save_as(row['Workbook'] + ' - ' + row['Branch'] + row['Format'])
+
+               
+
+                # Step 2: Get all the projects on server, then look for the
+                # right one.
+                all_projects, pagination_item = server.projects.get()
+                project = next((project for project in all_projects if project.name == row['Project']), None)
+
+
+                connection = ConnectionItem()
+                connection.server_address = args.server
+                connection.server_port = "443"
+                connection.connection_credentials = ConnectionCredentials(args.username, password, True)
+
+                all_connections = list()
+                all_connections.append(connection)
+
+                # Step 3: If project is found, form a new workbook item and
+                # publish.
+                if project is not None:
+                    new_workbook = sourceWB
+                    if args.as_job:
+                        new_job = server.workbooks.publish(new_workbook, row['Project'], overwrite_true,
+                                                           connections=all_connections, as_job=args.as_job)
+                        print("Workbook published. JOB ID: {0}".format(new_job.id))
+                    else:
+                        new_workbook = server.workbooks.publish(new_workbook, row['Project'], overwrite_true,
+                                                                connections=all_connections, as_job=args.as_job)
+                        print("Workbook published. ID: {0}".format(new_workbook.id))
+                else:
+                    error = "The default project could not be found."
+                    raise LookupError(error)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
