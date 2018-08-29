3daff1872d11a9c8e7e4f0ec85e09004aa1bb5ca
Sam Bao <sam.bao@onware.com>
2018-08-29 15:31:26 -0600

    TSC-152 #time 3h enabling showtabs on workbook view

--- samples/filter-and-replicate/filter-and-replicate.py
+++ samples/filter-and-replicate/filter-and-replicate.py
@@ -10,7 +10,10 @@ import tableauserverclient as TSC
 import os
 from tableauserverclient import ConnectionCredentials, ConnectionItem
 from tableaudocumentapi import Workbook
-
+from dateutil.parser import parse
+import platform
+import datetime
+import time
 
 ############################################################
 # Step 3) Use a database list (in CSV), loop thru and
@@ -54,85 +57,106 @@ def main():
     count = 1
     overwrite_true = TSC.Server.PublishMode.Overwrite
 
-    
+    # Open the workbook
+    source = "Source"
+    zip = "Zip"
+    export = "Export"
+    columns = ['Workbook', 'Format', 'Project', 'Branch', 'Directory', 'PublishDate','ModifiedDate' ]
 
     with server.auth.sign_in(tableau_auth):
-        with open('databases.csv') as csvfile:
+        with open('databases.csv',mode='r') as csvfile:
             total = sum(1 for line in open('databases.csv'))
-            print("Total file count: " + str(total))
-            databases = csv.DictReader(csvfile, delimiter=',', quotechar='"')         
-            for row in databases:
-               
-                print ("working on item: "+ str(count)+" \n")
-                # Open the workbook
-                source = "Source"
-                zip = "Zip"
-                export = "Export"
-                count = count +1 
+            print("Total file count: " + str(total-1))
+            databases = csv.DictReader(csvfile, delimiter=',', quotechar='"')    
+            outfile = open('databases_new.csv',mode='wb') 
+            writer = csv.DictWriter(outfile,fieldnames=columns)
+            writer.writeheader()
+            for row in databases:               
+                if any(row):
+                    print ("working on item: "+ str(count)+" \n")
+                    mtime = parse(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(os.path.getmtime(os.path.join(source, row['Workbook'] + ".twbx")))))
+                    ptime = parse(row['PublishDate'])          
+                    count = count +1 
+                    if(mtime>ptime):                             
+                        if(row['Format'] == ".twbx"):
+                            originaltableauworkbook = zipfile.ZipFile(os.path.join(source, row['Workbook'] + ".twbx"))                  
+                            originaltableauworkbook.extractall(zip)
+                            test = os.listdir(zip)
 
-                if(row['Format'] == ".twbx"):
-                    originaltableauworkbook = zipfile.ZipFile(os.path.join(source, row['Workbook'] + ".twbx"))                  
-                    originaltableauworkbook.extractall(zip)
-                    test = os.listdir(zip)
+                            for item in test:
+                                if item.endswith(".twb") :
+                                    sourceWB = Workbook(os.path.join(zip, item))
+                                    for datasource in reversed(sourceWB.datasources):
+                                        # Update the filters
+                                        for children in datasource._datasourceXML._children:
+                                            if "column" in children.attrib and "class" in children.attrib:
+                                                if children.attrib["column"] == "[Branch]" and children.attrib["class"] == "categorical":
+                                                    for subchildren in children._children:
+                                                        if "member" in subchildren.attrib:
+                                                            subchildren.attrib["member"] = '"' + row['Branch'] + '"'
 
-                    for item in test:
-                        if item.endswith(".twb") :
-                            sourceWB = Workbook(os.path.join(zip, item))
+                                             
+                                    # Save our newly created workbook with the new file name
+                                    outputpath = os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb")
+                                    sourceWB.save_as(outputpath) 
+                                    
+                                    if(row['Format'] == ".twbx"):
+                                        z = zipfile.ZipFile(os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']),mode='w',compression=zipfile.ZIP_DEFLATED)                                  
+                                        z.write(os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb"),item.replace("Export",""))
+                                        for item in list_files(zip):                                       
+                                            if item.endswith(".twb") != True:
+                                                z.write(item,item.replace("Zip",""))
+
+                                        z.close()
+
+                        elif (row['Format'] ==".twb"):
+                            sourceWB = Workbook(os.path.join(source, row['Workbook'] + ".twb"))                        
+                            # Update the filters
                             for datasource in reversed(sourceWB.datasources):
-                                # Update the filters
-                                for children in datasource._datasourceXML._children:
+                                for children in datasource._datasourceTree._root._children:
                                     if "column" in children.attrib and "class" in children.attrib:
                                         if children.attrib["column"] == "[Branch]" and children.attrib["class"] == "categorical":
                                             for subchildren in children._children:
                                                 if "member" in subchildren.attrib:
-                                                    subchildren.attrib["member"] = '"' + row['Branch'] + '"'
-
+                                                   subchildren.attrib["member"] = '"' + row['Branch'] + '"'
                                      
-                            # Save our newly created workbook with the new file name
-                            outputpath = os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb")
-                            sourceWB.save_as(outputpath) 
-                            
-                            if(row['Format'] == ".twbx"):
-                                z = zipfile.ZipFile(os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']),mode='w',compression=zipfile.ZIP_DEFLATED)                                  
-                                z.write(os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb"),item.replace("Export",""))
-                                for item in list_files(zip):                                       
-                                    if item.endswith(".twb") != True:
-                                        z.write(item,item.replace("Zip",""))
-
-                                z.close()
-
-                elif (row['Format'] ==".twb"):
-                    sourceWB = Workbook(os.path.join(source, row['Workbook'] + ".twb"))                        
-                    # Update the filters
-                    for datasource in reversed(sourceWB.datasources):
-                        for children in datasource._datasourceTree._root._children:
-                            if "column" in children.attrib and "class" in children.attrib:
-                                if children.attrib["column"] == "[Branch]" and children.attrib["class"] == "categorical":
-                                    for subchildren in children._children:
-                                        if "member" in subchildren.attrib:
-                                           subchildren.attrib["member"] = '"' + row['Branch'] + '"'
-                             
-                # Save our newly created workbook with the new file name
-                outputpath = os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb")
-                sourceWB.save_as(outputpath) 
+                        # Save our newly created workbook with the new file name
+                        outputpath = os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + ".twb")
+                        sourceWB.save_as(outputpath) 
 
 
-                all_projects, pagination_item = server.projects.get()
-                default_project = next((project for project in all_projects if project.name == row['Project']), None)              
-                
-                if default_project is not None:
-                    new_workbook = TSC.WorkbookItem(default_project.id,show_tabs= True)
-                    if args.as_job:
-                        new_job = server.workbooks.publish(new_workbook,os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']), overwrite_true)
-                        print("Workbook published. JOB ID: {0}".format(new_job.id))
-                    else:
-                        new_workbook = server.workbooks.publish(new_workbook, os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']), overwrite_true)
-                        print("Workbook published. ID: {0}".format(new_workbook.id))
-                else:
-                    error = "The default project could not be found."
-                    raise LookupError(error)
+                        all_projects, pagination_item = server.projects.get()
+                        default_project = next((project for project in all_projects if project.name == row['Project']), None)              
+                        
+                        if default_project is not None:
+                            new_workbook = TSC.WorkbookItem(default_project.id,show_tabs= True)
+                            if args.as_job:
+                                new_job = server.workbooks.publish(new_workbook,os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']), overwrite_true)
+                                row['PublishDate'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                writer.writerow(row)
+                                print("Workbook published. JOB ID: {0}".format(new_job.id))
+                            else:
+                                row['PublishDate'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                writer.writerow(row)
+                                new_workbook = server.workbooks.publish(new_workbook, os.path.join(export, row['Workbook'] + ' - ' + row['Branch'] + row['Format']), overwrite_true)
+                                print("Workbook published. ID: {0}".format(new_workbook.id))
+
+                        else:
+                            error = "The default project could not be found."
+                            raise LookupError(error)
     print ("Done!!!!!!!!!!!!!!!!!!")
-    
+    outfile.close()
+    statinfo = os.stat('databases_new.csv')
+    if(statinfo.st_size>67):
+        os.remove('databases.csv')
+        os.rename('databases_new.csv','databases.csv')
+    else:
+         os.remove('databases_new.csv')
+
+
+
+
+
     
 if __name__ == '__main__':
     main()
